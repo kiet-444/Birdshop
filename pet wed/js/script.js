7f6af44c0fdfbc98eaa70d7fff8cacd5eaadf506
Kiet Anh <70312316+kiet-444@users.noreply.github.com>
2023-06-06 09:59:24 +0700

    Add files via upload

---
+++ pet wed/js/script.js
@@ -0,0 +1,65 @@
+let closer = document.querySelector('#closer');
+
+closer.onclick = () => {
+    closer.style.display = 'none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    closer.style.display = "block";
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    closer.style.display = "block";
+    cart.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    closer.style.display = "block";
+    loginForm.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
